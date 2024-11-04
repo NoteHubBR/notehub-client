e27bd84af7960a5d763888cbd06bb2f810218a05
Lucas Aguiar <vintesetelucas@gmail.com>
2024-11-04 12:00:03 -0300

    🎉 init: initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'note-title': ['Playwrite GB S', 'sans-serif']
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
