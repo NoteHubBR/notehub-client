072f5a4982669ff73ba39033309cc81629d8f0dc
Lucas Aguiar <vintesetelucas@gmail.com>
2025-02-06 17:24:20 -0300

    ✨ feat(ux): display original time and date on relative date hover
    ♻ refactor: migrate Notification.tsx elements to semantic HTML elements

--- src/components/desktop/navbar/dropdown/elements/Notification.tsx
+++ src/components/desktop/navbar/dropdown/elements/Notification.tsx
@@ -1,13 +1,13 @@
 import { IconFlame } from "@tabler/icons-react";
 import { Notification as PropsType, toRelativeTime, Type } from "@/core";
 import { Photo } from "@/components/Photo";
+import { useCallback, useState } from "react";
 import Link from "next/link";
 
 export const Notification = ({ notification }: { notification: PropsType }) => {
 
     const {
         from_user: user,
-        read,
         info: {
             type,
             target,
@@ -15,37 +15,71 @@ export const Notification = ({ notification }: { notification: PropsType }) => {
         }
     } = notification;
 
-    const href = type === Type.FOLLOWER ? `/${user.username}` : `/${user.username}/${target}`;
+    const getHref = (type: Type): string => {
+        if (type === Type.FOLLOWER) return `/${user.username}`;
+        else return `/${user.username}/${target}`;
+    }
 
-    const emote: string | null = type === Type.FOLLOWER ? '👀' : Type.COMMENT ? '🗣' : Type.REPLY ? '🗣' : null;
+    const getEmote = (type: Type): string | null => {
+        switch (type) {
+            case Type.FOLLOWER: return '👀';
+            case Type.COMMENT:
+            case Type.REPLY: return '🗣';
+            default: return null;
+        }
+    }
 
+    const href = getHref(type);
+    const emote = getEmote(type);
+    const relativeTime = toRelativeTime(notification.created_at);
     const [username, message] = [
         text.split(' ').filter(word => word.startsWith('@')),
         text.split(' ').filter(word => !word.startsWith('@')).join(' ')
     ]
 
-    const relativeTime = toRelativeTime(notification.created_at);
+    const [isMouseOnDateField, setIsMouseOnDateField] = useState<boolean>(false);
+    const handleMouseEnter = useCallback(() => setIsMouseOnDateField(true), []);
+    const handleMouseLeave = useCallback(() => setIsMouseOnDateField(false), []);
 
     return (
         <Link
             href={`${href}`}
-            className={`py-2 hover:dark:bg-neutral-50/10 hover:bg-neutral-900/10 transition-colors ${read ? '' : 'dark:bg-neutral-50/10 bg-neutral-900/10'}`}
+            className={`py-2 hover:dark:bg-neutral-50/10 hover:bg-neutral-900/10 transition-colors`}
         >
-            <div className="w-full flex items-center">
-                <div className="px-2 border-r text-sm dark:border-r-neutral-50/20 border-r-neutral-900/20">
-                    <Photo user={user} size={55} />
-                </div>
-                <div className="px-2">
-                    <p>
-                        <span>{emote ? emote : <IconFlame fill="#6d28d9" color="#7c3aed" />} </span>
-                        <span className="font-semibold text-violet-600">{username} </span>
-                        {message}
+            <article className="w-ull">
+                <main className="flex items-center">
+                    <figure className="px-2 border-r text-sm dark:border-r-neutral-50/20 border-r-neutral-900/20">
+                        <Photo user={user} size={55} />
+                    </figure>
+                    <section className="px-2">
+                        <p>
+                            <span>{emote ? emote : <IconFlame fill="#6d28d9" color="#7c3aed" />} </span>
+                            <span className="font-semibold text-violet-600">{username} </span>
+                            {message}
+                        </p>
+                    </section>
+                </main>
+                <footer className="w-full flex justify-end">
+                    <p
+                        className="flex gap-2 px-2 text-xs dark:text-neutral-50/50 text-neutral-900/50"
+                    >
+                        <span
+                            className={`
+                            ${isMouseOnDateField ? 'opacity-100' : 'opacity-0'} 
+                            px-2 rounded-full 
+                            font-semibold dark:text-white text-black
+                            dark:bg-black bg-white
+                            transition-opacity duration-200
+                        `}
+                        >
+                            {notification.created_at}h
+                        </span>
+                        <span onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                            {relativeTime}
+                        </span>
                     </p>
-                </div>
-            </div>
-            <div className="w-full flex justify-end">
-                <span className="pr-2 text-xs dark:text-neutral-50/50 text-neutral-900/50">{relativeTime}</span>
-            </div>
+                </footer>
+            </article>
         </Link>
     )
 
