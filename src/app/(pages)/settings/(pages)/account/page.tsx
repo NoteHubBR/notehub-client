a81829707914d38defe17db2aa4d4891da1ba2f4
Lucas Aguiar <vintesetelucas@gmail.com>
2025-06-30 17:10:51 -0300

    ✨ feat(mobile): add logout option to user settings page

--- src/app/(pages)/settings/(pages)/account/page.tsx
+++ src/app/(pages)/settings/(pages)/account/page.tsx
@@ -1,10 +1,14 @@
 'use client';
 
 import { Header } from "../Header";
-import { IconCancel, IconEye, IconKey, IconMail, IconUser } from "@tabler/icons-react";
+import { IconCancel, IconEye, IconKey, IconLogout, IconMail, IconUser, IconUsers } from "@tabler/icons-react";
 import { Link } from "./Link";
+import { useScreen, useUser } from "@/data/hooks";
 
 const Page = () => {
+
+    const { onMobile } = useScreen();
+    const { clearUser } = useUser();
 
     return (
         <section>
@@ -16,6 +20,12 @@ const Page = () => {
                     <Link href={"/settings/account/email"} icon={IconMail}>Alterar email</Link>
                     <Link href={"/settings/account/password"} icon={IconKey}>Alterar senha</Link>
                     <Link href={"/settings/account/delete"} icon={IconCancel}>Deletar conta</Link>
+                    {onMobile &&
+                        <>
+                            <Link href={"/signin"} icon={IconUsers}>Mudar de conta</Link>
+                            <Link onClick={clearUser} href={"/"} icon={IconLogout}>Sair</Link>
+                        </>
+                    }
                 </ul>
             </section>
         </section>
--- src/app/(pages)/settings/layout.tsx
+++ src/app/(pages)/settings/layout.tsx
@@ -2,17 +2,20 @@
 
 import { Element } from "./elements";
 import { usePathname } from "next/navigation";
-import { useScreen } from "@/data/hooks";
+import { useScreen, useUser } from "@/data/hooks";
+import { NotFound } from "./NotFound";
 
 const Layout = (props: any) => {
 
     const pathname = usePathname();
     const onPathname = pathname === "/settings";
+
     const { onDesktop } = useScreen();
+    const { user } = useUser();
 
     const { Aside, Main } = Element;
 
-    return (
+    if (user) return (
         <section className="w-full h-full px-4 inmd:px-0 flex items-center justify-center dark:bg-darker bg-lighter">
             <div className="max-w-[999px] w-full min-h-[75%] inmd:h-full flex">
                 <Aside onDesktop={onDesktop} onPathname={onPathname} />
@@ -22,6 +25,8 @@ const Layout = (props: any) => {
             </div>
         </section>
     )
+
+    return <NotFound />;
 
 }
 
