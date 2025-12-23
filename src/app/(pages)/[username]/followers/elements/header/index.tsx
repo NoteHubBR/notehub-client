3a6689c5bb4a0dcac9269ecf2c5ee284273f3dd0
Lucas Aguiar <vintesetelucas@gmail.com>
2025-12-23 14:36:20 -0300

    refactor(ui): correct placeholder text

--- src/app/(pages)/[username]/followers/elements/header/index.tsx
+++ src/app/(pages)/[username]/followers/elements/header/index.tsx
@@ -7,7 +7,7 @@ export const Header = (props: React.HTMLAttributes<HTMLElement>) => {
         <header className="pb-4 border-b dark:border-neutral-700/50 border-dark/25" {...props}>
             <nav>
                 <ul className="flex items-center justify-between inlg:justify-center gap-2 flex-wrap">
-                    <Element.Input placeholder="Encontrar uma nota..." />
+                    <Element.Input placeholder="Encontrar uma pessoa..." />
                     <Element.Sorter orderParam="order" sortParam="sort" orderValues={["followersCount", null]} sortValues={["desc", null]}>Seguidores</Element.Sorter>
                     <Element.Sorter orderParam="order" sortParam="sort" orderValues={["username"]} sortValues={["asc"]}>A-Z</Element.Sorter>
                     <Element.Sorter orderParam="order" sortParam="sort" orderValues={["username"]} sortValues={["desc"]}>Z-A</Element.Sorter>
