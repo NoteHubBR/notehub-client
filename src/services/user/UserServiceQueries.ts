b6a8efa5ff0b052413fea843f37faa57284913bf
Lucas Aguiar <vintesetelucas@gmail.com>
2026-03-04 10:53:34 -0300

    refactor(user): replace manual fetch logic with react-query

--- src/services/user/UserServiceQueries.ts
+++ src/services/user/UserServiceQueries.ts
@@ -1,9 +1,34 @@
+import { customQueryFn, customRetry } from '../utils';
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { UserService } from './UserService';
 
 export const UserServiceQueries = () => {
 
     const service = UserService();
+
+    // searchUserFollowing searchUserFollowers
+
+    const useGetUser = (username: string, enabled: boolean = true) => {
+        return useQuery({
+            queryKey: ['user', username],
+            queryFn: customQueryFn(() => service.getUser(username)),
+            enabled: enabled,
+            staleTime: 1000 * 60 * 5,
+            placeholderData: keepPreviousData,
+            retry: customRetry
+        })
+    }
+
+    const useGetUserDisplayNameHistory = (username: string, enabled: boolean = true) => {
+        return useQuery({
+            queryKey: ['getUserDisplayNameHistory', username],
+            queryFn: customQueryFn(() => service.getUserDisplayNameHistory(username)),
+            enabled: enabled,
+            staleTime: 1000 * 60 * 5,
+            placeholderData: keepPreviousData,
+            retry: customRetry
+        })
+    }
 
     const useSearchUsers = (parameters?: string, enabled: boolean = true) => {
         return useQuery({
@@ -17,6 +42,8 @@ export const UserServiceQueries = () => {
     }
 
     return {
+        useGetUser,
+        useGetUserDisplayNameHistory,
         useSearchUsers
     }
 
