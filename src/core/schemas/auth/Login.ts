5e05794a5c7abe139efc14df71aefe73f4362c99
Lucas Aguiar <vintesetelucas@gmail.com>
2026-03-24 09:40:37 -0300

    feat(auth): allow login with email or username

--- src/core/schemas/auth/Login.ts
+++ src/core/schemas/auth/Login.ts
@@ -2,15 +2,14 @@ import { noForbiddenWords } from '@/core/utils';
 import { z } from 'zod';
 
 export const loginFormSchema = z.object({
-    username: noForbiddenWords("Não pode")(z
+    identifier: noForbiddenWords("Não pode")(z
         .string().trim().toLowerCase()
-        .regex(/^[a-zA-Z0-9_.-]+$/, "Use letras, números, _, . ou -")
         .min(2, 'Mínimo de 2 caracteres.')
-        .max(12, 'Máximo de 12 caracteres.')),
+        .max(255, 'Máximo de 255 caracteres.')),
     password: z
         .string().trim()
         .min(4, 'Mínimo de 4 caracteres.')
         .max(255, 'Máximo de 255 caracteres.'),
-});
+})
 
 export type LoginFormData = z.infer<typeof loginFormSchema>;
